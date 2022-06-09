30812b7a186f9783c19c46a024bdc77e2c471b2a
abbos <abbosp09@gmail.com>
2022-06-09 21:46:13 +0500

    add exporter for metric

--- service/application/app/index.ts
+++ service/application/app/index.ts
@@ -1,7 +1,28 @@
 import * as express from 'express';
 import 'express-async-errors';
 
+const client = require('prom-client');
+const express = require('express');
 const app = express();
+
+// Create a registry and pull default metrics
+const register = new client.Registry();
+
+client.collectDefaultMetrics({
+    app: 'node-application-monitoring-app',
+    prefix: 'node_',
+    timeout: 10000,
+    gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
+    register
+});
+
+app.get('/metrics', async (req, res) => {
+    res.setHeader('Content-Type', register.contentType);
+    res.send(await register.metrics());
+});
+
+//////////////////////////////////////////////////////////////
+// Other get responce info
 
 app.get('*', (req: express.Request, res: express.Response) => {
   const response = {
@@ -13,6 +34,4 @@ app.get('*', (req: express.Request, res: express.Response) => {
   res.status(200).send(response);
 });
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
-});
+app.listen(3000, () => console.log('Server is running on http://localhost:3000, metrics are exposed on http://localhost:3000/metrics'));
